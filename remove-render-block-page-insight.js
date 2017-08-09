ba483c9118e7878050a32c41e59847a15d5171e4
lozinskydino <adso.menezes@gmail.com>
2017-08-09 14:11:15 -0300

    Script to remove render block
    Sometimes the page insights from google warns it in their test, using this script you can remove this blocking from css.

---
+++ remove-render-block-page-insight.js
@@ -0,0 +1,14 @@
+<script>
+	//Create variable to create element
+	var datepicker = document.createElement('link');
+	//Set Rel attribute
+	bootstrap.rel = 'stylesheet';
+	//Set source attribute
+	bootstrap.href = 'url css';
+	//Set type attribute
+	bootstrap.type = 'text/css';
+	//Get some link tag that exists
+	var bootster = document.getElementsByTagName('link')[0];
+	//Insert the link within head
+	bootster.parentNode.insertBefore(bootstrap, bootster);
+</script>
